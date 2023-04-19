f49f75e67bb45366c37699f95d65c092720113eb
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-04-18 22:49:37 -0400

    nlp preprocessor

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -33,6 +33,3 @@ def preprocess_and_chunk(text):
             chunks.append(token.text)
 
     return chunks
-
-
-print(preprocess_and_chunk("Gates was born and raised in Seattle. In 1975, he and Allen founded Microsoft in Albuquerque, New Mexico. It became the world's largest personal computer software company.[5][a] Gates led the company as chairman and CEO until stepping down as CEO in January 2000, succeeded by Steve Ballmer, but he remained chairman of the board of directors and became chief software architect.[8] During the late 1990s, he was criticized for his business tactics, which have been considered anti-competitive. This opinion has been upheld by numerous court rulings.[9] In June 2008, Gates transitioned to a part-time role at Microsoft and full-time work at the Bill & Melinda Gates Foundation, the private charitable foundation he and his then-wife Melinda established in 2000.[10] He stepped down as chairman of the board of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella.[11] In March 2020, Gates left his board positions at Microsoft and Berkshire Hathaway to focus on his philanthropic efforts on climate change, global health and development, and education.[12]"))
