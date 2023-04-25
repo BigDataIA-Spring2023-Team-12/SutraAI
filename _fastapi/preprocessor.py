59483f90565d45a835452db8adab8ad6314ef14f
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-04-25 10:40:55 -0400

    fastapi -> utils

---
+++ _fastapi/preprocessor.py
@@ -0,0 +1,39 @@
+import spacy
+
+# Load the spaCy model
+nlp = spacy.load('en_core_web_sm')
+
+
+def preprocess_and_chunk(text):
+    """
+    This function takes a string as input, performs NLP preprocessing using spaCy,
+    removes stop words, lemmatizes the text, and chunks it into meaningful phrases
+    using Named Entity Recognition (NER), dependency parsing, and noun chunking.
+
+    Parameters:
+    text (str): Input text to preprocess and chunk.
+
+    Returns:
+    chunks (list): List of chunks generated from the input text.
+    """
+    # Perform NLP preprocessing
+    doc = nlp(text)
+
+    # Remove stop words, punctuation marks, and lemmatize the text
+    tokens = [token.lemma_.lower() for token in doc if not token.is_stop and not token.is_punct]
+    print(tokens)
+
+    # Convert tokens list to sentence
+    sentence = nlp(' '.join(tokens))
+
+    # Chunk the text into meaningful phrases using NER, dependency parsing, and noun chunking
+    chunks = []
+    for chunk in sentence.noun_chunks:
+        chunks.append(chunk.text)
+    for token in doc:
+        if token.ent_type_ and token.ent_iob == 3:
+            chunks.append(token.text)
+        elif token.dep_ in ('ROOT', 'conj', 'appos'):
+            chunks.append(token.text)
+
+    return chunks
